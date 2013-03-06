def8cbb58d6946097c4afffe851e700244e3dbd2
wemstar <sylwestermacura@gmail.com>
2013-03-06 18:42:39 +0100

    Nowszy Test

--- Lab01_1/src/LabiryntTest.java
+++ Lab01_1/src/LabiryntTest.java
@@ -8,7 +8,12 @@ public class LabiryntTest {
 		Labirynt labirynt=new Labirynt(10,10);
 		
 		labirynt.dodajPrzeszkode(1,1,5,false);
+		labirynt.dodajPrzeszkode(6,0,5,true);
 		labirynt.dodajPrzeszkode(3, 4, 2, true);
+		labirynt.wypisz();
+		Podroznik Indiana=new Podroznik(labirynt);
+		
+		Indiana.start();
 		labirynt.wypisz();
 		
 

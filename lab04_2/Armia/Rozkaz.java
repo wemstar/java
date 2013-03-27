5534dfc7ac399f6bec9910ca084cc3f65bc0d13f
wemstar <sylwestermacura@gmail.com>
2013-03-27 20:10:53 +0100

    Plik Zawiera klase rozkaz odpowiedzalna za przychowywanie rozkazow

---
+++ lab04_2/Armia/Rozkaz.java
@@ -0,0 +1,19 @@
+package Armia;
+
+public class Rozkaz  {
+	public Rozkaz(String str){
+		polecenie=str;
+	}
+
+
+	public String toString()
+	{
+		return polecenie+"\n";
+	}
+
+
+
+
+
+	private String polecenie;
+}

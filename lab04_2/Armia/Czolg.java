8ffd226a4301f8a9a80db299be85ec51d67627f8
wemstar <sylwestermacura@gmail.com>
2013-03-27 20:12:12 +0100

    plik zawiera klase Czolg ktora reprezentuje jednostke pancerna

---
+++ lab04_2/Armia/Czolg.java
@@ -0,0 +1,29 @@
+package Armia;
+import java.util.*;
+public class Czolg  {
+
+
+	public String ostatniRozkaz(){
+		return new String("Ostatni Rozkaz: ")+rozkazy.lastElement().toString();
+	}
+	public void dodajRozkaz(Rozkaz polecenie)
+	{
+		rozkazy.add(polecenie);
+
+	}
+	public String toString()
+	{
+		StringBuilder lista=new StringBuilder();
+
+
+		for (Rozkaz roz : rozkazy) {
+			lista.append(roz);
+			
+		}
+		return lista.toString();
+	}
+
+
+	private Vector<Rozkaz> rozkazy=new Vector<Rozkaz>();
+	
+}

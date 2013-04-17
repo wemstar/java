b8634533cd73f8bcca13f64bf030188d97900e0f
wemstar <sylwestermacura@gmail.com>
2013-04-17 22:29:34 +0200

    szablon dla algorytmów pastwiskowych

---
+++ lab07_02/Pastwisko.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+
+class Pastwisko {
+
+	public Pastwisko(int x,int y){}
+	public void add(Ssak zwierze){
+		zwierzyniec.add(zwierze);
+	}
+
+
+
+
+
+	private Vector<Ssak> zwierzyniec=new Vector<Ssak>();
+
+	
+}
---
+++ lab07_02/lab07.java
@@ -0,0 +1,16 @@
+
+class lab07 {
+	
+	public static void main(String[] args) {
+		
+
+		Pastwisko pole=new Pastwisko(20,20);
+		pole.add(new Owca());
+		pole.add(new Owca());
+		pole.add(new Owca());
+		pole.add(new Owca());
+		pole.add(new Owca());
+		pole.add(new Wilk());
+		pole.add(new Wilk());
+	}
+}

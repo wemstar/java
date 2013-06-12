44cd92df6a2bafcd94553f659f72d8213d7dd0b8
Wemstar <sylwestermacura@gmail.com>
2013-06-12 19:51:41 +0200

    Lab12 z 6.06.2103

---
+++ lab12/lab12.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author i1macura
+ */
+public class lab12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+}
